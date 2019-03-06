acd103dbb3e70c6ba8ff8d0bb6339cf327c29298
Arthur A. Salazar <47439983+ArthurASalazar@users.noreply.github.com>
2019-03-05 19:44:38 -0800

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,42 @@
+#include "person.h"
+#include <iostream>
+#include <string>
+using namespace std;
+Person::Person(){
+
+}
+void Person::setLastName(string lName)
+{
+ lastName = lName;
+}
+string Person::getLastName(){
+  return lastName;
+}
+void Person::setFirstName(string fName){
+ firstName = fName;
+}
+string Person::getFirstName(){
+  return firstName;
+}
+void Person::setPayRate(float rate){
+ payRate = rate;
+}
+float Person::getPayRate(){
+  return payRate;
+}
+void Person::setHoursWorked(float hours){
+ hoursWorked = hours;
+}
+float Person::getHoursWorked(){
+  return hoursWorked;
+}
+float Person::totalPay(){
+  float total;
+  total= payRate * hoursWorked;
+  return total;
+}
+string Person::fullName(){
+  string fullname;
+  fullname = firstName +" " +lastName;
+  return fullname;
+}
